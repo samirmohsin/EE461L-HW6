5bfbb961d5b566d971d2bc2d2ccc4fa3c008b4e2
Samir Mohsin <samir.smohsin@utexas.edu>
2022-03-28 22:22:39 -0500

    changed static folder path

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ from flask import Flask,render_template,request,jsonify
 from flask.helpers import send_from_directory
 #from flask_cors import CORS
 
-app = Flask(__name__, static_folder="frontend/build", static_url_path="")
+app = Flask(__name__, static_folder="./build", static_url_path="/")
 
 #CORS(app)
 
@@ -34,4 +34,4 @@ def index():
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=False, port=os.environ.get('PORT', 80))
